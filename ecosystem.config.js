61e78f27ecc8004fe16f18b35a6c2189de98e23e
nilpatel <nilpatel62@gmail.com>
2026-08-14 10:04:14 +0530

    Update lead-service configuration: modify args to include host and set working directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,9 @@ module.exports = {
     apps: [{
         name: 'lead-service',
         script: '/opt/python_apis/lead/bin/python',
-        args: 'uvicorn app.main:app --reload --port 8002',
+        args: '-m uvicorn app.main:app --host 0.0.0.0 --port 8002',
+        interpreter: 'none',
+        cwd: '/opt/python_apis/wapnexus-lead-generation',
         instances: 1,
         autorestart: true,
         watch: false,
