1666b8e7292c564e59ca2a03caccf951c2c4aa22
nilpatel <nilpatel62@gmail.com>
2026-08-14 10:02:03 +0530

    Add ecosystem configuration for whatsapp-service

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [{
+        name: 'whatsapp-service',
+        script: '/opt/python_apis/lead/bin/python',
+        args: 'uvicorn app.main:app --reload --port 8002',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G'
+    }]
+};
